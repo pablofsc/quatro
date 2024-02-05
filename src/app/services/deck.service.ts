6916ae45db1f058e5048effdf351ca2c56771bd8
pablofsc <pablofsc@outlook.com>
2024-02-05 01:11:56 -0300

    fix: deck.json inaccessible when deployed

--- src/app/services/deck.service.ts
+++ src/app/services/deck.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 
-const deckPath = '../../assets/deck.json';
+let deckPath = '../../assets/deck.json';
+
+if (window.location.hostname.includes('github.io')) { // TODO: improve this
+  deckPath = '/quatro/assets/deck.json';
+
+  console.log('Running on GitHub Pages');
+}
+else {
+  console.log('Running locally');
+}
+
 
 export interface Card {
   type: CardType;
