fc35b74978a60c7150fb1d33eb668a7fd99e3711
Pablo Felipe <pablofsc@outlook.com>
2024-05-01 22:00:53 -0300

    simplify fade-in animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
 import { Screen, ScreensService } from 'src/app/services/screens.service';
-
-export const fadeInOutAnimation = trigger('fadeInOutAnimation', [
-  transition(':enter', [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 })),]),
-  transition(':leave', [animate('300ms', style({ opacity: 0 })),]),
-]);
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  animations: [fadeInOutAnimation],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   constructor(
