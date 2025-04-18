ea6212bd38e65ff7d611da31fd5ea93b64b908da
Pablo Felipe <pablofsc@outlook.com>
2025-04-18 19:50:20 -0300

    print hostname on AppComponent construction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,13 @@ export class AppComponent {
     public readonly screens: ScreensService,
   ) {
     console.log('Backend url is set to', environment.BACKEND_URL);
+
+    if (window.location.hostname.includes('github.io')) { // TODO: improve this
+      console.log('Project is running on GHP');
+    }
+    else {
+      console.log('Project is not running on GHP, running on:', window.location.hostname);
+    }
   }
 
   changeState(newScreen: Screen) {
